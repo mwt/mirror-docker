1fce9b306aa915860b8e7ecb4c66e34b64cc5533
Matthew W. Thomas <matthew@mwt.me>
2022-10-25 16:10:34 -0500

    ignore irrelevant apt-mirror files

--- apt-mirror.bash
+++ apt-mirror.bash
@@ -6,6 +6,7 @@ MIRROR_URL="mirror.mwt.me"
 
 SCRIPT_DIR="$(dirname "$0")"
 desination_path="/mnt/mirror/apt-mirror"
+mirror_path="/mnt/mirror/apt-mirror/mirror"
 
 . "${SCRIPT_DIR}/.secrets/cloudflare"
 
@@ -27,12 +28,12 @@ apt-mirror
 # CDN Purge
 ####################
 
-find "$desination_path" -type f -path '*dist*' -mmin -360 -print | sed \
- -e "s|^${desination_path}/packagecloud.io/shiftkey/desktop/any|ghd/deb|" \
- -e "s|^${desination_path}/grimler.se/termux-packages-24|termux/main|" \
- -e "s|^${desination_path}/grimler.se/termux-root-packages-24|termux/root|" \
- -e "s|^${desination_path}/grimler.se/x11-packages|termux/x11|" \
- -e "s|^${desination_path}/apt.retorque.re/file/zotero-apt|zotero/deb|" | 
+find "$mirror_path" -type f -path '*dist*' -mmin -360 -print | sed \
+ -e "s|^${mirror_path}/packagecloud.io/shiftkey/desktop/any|ghd/deb|" \
+ -e "s|^${mirror_path}/grimler.se/termux-packages-24|termux/main|" \
+ -e "s|^${mirror_path}/grimler.se/termux-root-packages-24|termux/root|" \
+ -e "s|^${mirror_path}/grimler.se/x11-packages|termux/x11|" \
+ -e "s|^${mirror_path}/apt.retorque.re/file/zotero-apt|zotero/deb|" | 
 while mapfile -t -n 30 ary && ((${#ary[@]}))
 do
     printf '%s\n' "${ary[@]}" | jq -R . | jq -s "{ \"files\" : map(\"https://${MIRROR_URL}/\" + .) }" | tee "$TMPFILE"
