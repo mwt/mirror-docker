676acb3814f04344fda5503cfe4b44410f2a42d7
Matthew W. Thomas <matthew@mwt.me>
2023-04-17 10:24:15 -0500

    Update default-install.ssi.sh

--- misc/default-install.ssi.sh
+++ misc/default-install.ssi.sh
@@ -5,11 +5,11 @@ BASE_URL="<!--# echo var='repo_base_url' -->"
 GPG_KEY="<!--# include file='./gpgkey' -->"
 
 DEB_REPO="$BASE_URL/deb any main"
-RPM_REPO="[$APP_NAME]
+RPM_REPO="[mwt-$APP_NAME]
 name=$APP_NAME
 baseurl=$BASE_URL/rpm
 enabled=1
-gpgcheck=1
+gpgcheck=0
 repo_gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/$APP_NAME.asc"
 
